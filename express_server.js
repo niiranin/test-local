1ade07a76b157dff0e7caf102b91ba300783daac
Niiranen Nina <nina.niiranen@tieto.com>
2018-04-19 12:21:12 +0300

    just another test

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-//testing changed to git
+//testing changed to git, and some more comments to test this with eclipse
 app.get('/blocks', function(request, response) {
 	var blocks = ['Fixed', 'Movable', 'Rotating'];
 	response.send(blocks);

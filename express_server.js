3aed4dfd4dffd896ccce916e219e83e2c53eb885
Niiranen Nina <nina.niiranen@tieto.com>
2018-04-19 11:34:23 +0300

    test

--- express_server.js
+++ express_server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
-
+//testing changed to git
 app.get('/blocks', function(request, response) {
 	var blocks = ['Fixed', 'Movable', 'Rotating'];
 	response.send(blocks);
